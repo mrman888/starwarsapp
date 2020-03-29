3873b02a168d1095458b79d7d83bf6550f1b21fb
mrman888 <khdmdcm@gmail.com>
2020-03-29 17:04:42 +0200

    added button

--- src/app/features/search/search.component.ts
+++ src/app/features/search/search.component.ts
@@ -1,17 +1,17 @@
-import { SearchResultFilm } from './../../shared/interfaces/search-result-film';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ChangeDetectorRef, AfterContentInit } from '@angular/core';
 import { Angular2SwapiService, Film } from 'angular2-swapi';
-import { Observable } from 'rxjs/internal/Observable';
+import { SearchResultFilm } from './../../shared/interfaces/search-result-film';
 
 @Component({
 	selector: 'app-search',
 	templateUrl: './search.component.html',
 	styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, AfterContentInit {
 	films: SearchResultFilm[] = [];
+	counter = 0;
 
-	constructor(private _swapi: Angular2SwapiService) {}
+	constructor(private cdRef: ChangeDetectorRef, private _swapi: Angular2SwapiService) {}
 
 	ngOnInit(): void {
 		this._swapi.getFilms().subscribe((filmsResponse: Film[]) => {
@@ -26,5 +26,23 @@ export class SearchComponent implements OnInit {
 
 	toggle(film: SearchResultFilm): void {
 		film.isFavorite = !film.isFavorite;
+		this.counter++;
 	}
+
+	getFavoriteText(isFavorite: boolean): string {
+		let text = '';
+		if (isFavorite) {
+			text = 'Favorite';
+		} else {
+			text = '';
+		}
+
+		this.counter++;
+		console.log(this.counter);
+		return text;
+	}
+
+	ok() {}
+
+	ngAfterContentInit() {}
 }
